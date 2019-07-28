9f2e20a29ab4e138a766c019e1959fbd895d9e5f
Marx JMoura <marx.jmoura@gmail.com>
2019-07-28 19:25:53 -0300

    Coming soon page

---
+++ src/components/Loader.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="loader">
+    <div class="circle circle-salad"></div>
+    <div class="circle circle-ketchup"></div>
+    <div class="circle circle-mustard"></div>
+  </div>
+</template>
+
+<script>
+import anime from 'animejs/lib/anime.es.js'
+
+export default {
+  name: 'loader',
+  mounted () {
+    var borderRadius = '12px'
+    var direction = 'alternate'
+    var loop = true
+    var elasticity = 400
+    var easing = 'easeInElastic'
+    var duration = 800
+    var delay = 600
+    var endDelay = 600
+    var height = '36px'
+    var width = '36px'
+
+    anime ({
+      targets: ['.circle-salad'],
+      direction: direction,
+      loop: loop,
+      elasticity: elasticity,
+      easing: easing,
+      duration: duration,
+      delay: delay,
+      endDelay: endDelay,
+      translateY: -18,
+      translateX: 24,
+      borderRadius: borderRadius,
+      height: height,
+      width: width
+    })
+
+    anime ({
+      targets: ['.circle-ketchup'],
+      direction: direction,
+      loop: loop,
+      elasticity: elasticity,
+      easing: easing,
+      duration: duration,
+      delay: delay,
+      endDelay: endDelay,
+      translateY: 22,
+      translateX: -18,
+      borderRadius: borderRadius,
+      height: height,
+      width: width
+    })
+
+    anime ({
+      targets: ['.circle-mustard'],
+      direction: direction,
+      loop: loop,
+      elasticity: elasticity,
+      easing: easing,
+      duration: duration,
+      delay: delay,
+      endDelay: endDelay,
+      translateY: -18,
+      translateX: -22,
+      borderRadius: borderRadius,
+      height: height,
+      width: width
+    })
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="d-flex flex-column">
+    <Loader class="mb-3"/>
+    <h1 class="text-center">
+      Storefront Community
+    </h1>
+    <p class="text-center">
+      Working to innovate the food retail experience
+    </p>
   </div>
 </template>
 
 <script>
+import Loader from '@/components/Loader'
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Loader
+  }
 }
 </script>
