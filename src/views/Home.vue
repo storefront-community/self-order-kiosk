9b09f545743a82fe868f57703dc29c908d5a69e8
Marx JMoura <marx.jmoura@gmail.com>
2019-07-29 13:58:16 -0300

    Show menu board after reaching idle timeout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="d-flex flex-column">
+    <IdleTime @timeout="showMenuBoard"/>
     <Loader class="mb-3"/>
     <h1 class="text-center">
       Storefront Community
@@ -11,11 +12,19 @@
 </template>
 
 <script>
+import IdleTime from '@/components/IdleTime'
 import Loader from '@/components/Loader'
 
 export default {
   name: 'home',
+  methods: {
+    showMenuBoard() {
+      const params = { contentId: 1 }
+      this.$router.push({ name: 'menuBoard', params: params })
+    }
+  },
   components: {
+    IdleTime,
     Loader
   }
 }
