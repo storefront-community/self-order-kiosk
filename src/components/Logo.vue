1d82153f80b4dad0cb32556244713bd72c6ba1a4
Marx JMoura <marx.jmoura@gmail.com>
2019-08-02 17:58:22 -0300

    Control size of Logo component

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,19 @@
 <template>
-  <img src="/img/storefront-community.svg" height="32px">
+  <img src="/img/storefront-community.svg" :height="height">
 </template>
 
 <script>
 export default {
-  name: 'logo'
+  name: 'logo',
+  props: ['size'],
+  computed: {
+    height() {
+      switch (this.size) {
+        case 'x-large': return '92px'
+        case 'large': return '64px'
+        default: return '32px'
+      }
+    }
+  }
 }
 </script>
