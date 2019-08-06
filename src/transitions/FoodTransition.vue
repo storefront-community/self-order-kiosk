ea58e968a163391aa82df448ccbcfa24ee5b5ce8
Marx JMoura <marx.jmoura@gmail.com>
2019-08-06 14:52:10 -0300

    Add transition to the food in the choice list

---
+++ src/transitions/FoodTransition.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition-group @enter="enter" appear>
+    <slot></slot>
+  </transition-group>
+</template>
+
+<script>
+import { Power4, TimelineMax } from "gsap/TweenMax"
+
+export default {
+  name: 'foodTransition',
+  methods: {
+    enter(el, done) {
+      const timeLineMax = new TimelineMax({
+        onComplete: done
+      })
+
+      timeLineMax.set(el, {
+        autoAlpha: 0,
+        scale: .2,
+        transformOrigin: '50% 50%'
+      })
+
+      timeLineMax.to(el, .5, {
+        delay: .25,
+        autoAlpha: 1,
+        scale: 1,
+        ease: Power4.easeOut
+      })
+    }
+  }
+}
+</script>
