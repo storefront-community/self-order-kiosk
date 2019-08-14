24d405c5e1620466b6d9ed222910dfd162ec71b5
Marx JMoura <marx.jmoura@gmail.com>
2019-08-14 13:44:54 -0300

    Optional list in step to customize food

---
+++ src/views/foods/CustomizeFood.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="app-body" v-if="hasOptionals">
+    <div class="app-header">
+      <div class="container d-flex align-items-center">
+        <div class="rounded-clipping mr-3">
+          <img :src="item.imageUrl">
+        </div>
+        <h1 class="display-3 text-right">
+          {{ item.name }}<br>
+          <small class="text-primary">
+            <small>
+              <sup>$</sup>
+            </small>
+            {{ item.price | numeral }}
+          </small>
+        </h1>
+      </div>
+    </div>
+    <div class="app-content">
+      <div class="container">
+        <SlideTransition :direction="slide">
+          <Optional :optional="optional" :key="optional.id" />
+        </SlideTransition>
+      </div>
+    </div>
+    <div class="app-footer">
+      <div class="container d-flex">
+        <button type="button" class="btn btn-outline-primary btn-lg mr-auto" @click="back">
+          Back
+        </button>
+        <button type="button" class="btn btn-primary btn-lg ml-auto" @click="next">
+          <span class="mr-3">
+            {{ currentPage }} of {{ numberOfPages }}
+          </span>
+          <i class="fa fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Optional from './Optional'
+import SlideTransition from '@/transitions/SlideTransition'
+import waitTransition from '@/hacks/waitTransition'
+
+export default {
+  name: 'customizeFood',
+  components: {
+    Optional,
+    SlideTransition
+  },
+  async mounted() {
+    if (!this.$state.initialized) return
+
+    this.optionals = await this.$api.optionals.list(this.$state.category.id)
+    this.hasOptionals = !!this.optionals.length
+
+    if (!this.hasOptionals) {
+      waitTransition(() => this.next())
+    }
+  },
+  data() {
+    return {
+      hasOptionals: false,
+      optionals: [],
+      currentIndex: 0,
+      slide: 'left'
+    }
+  },
+  methods: {
+    addItemToOrder() {
+      this.$router.push({ name: 'orderSummary' })
+    },
+    back() {
+      if (this.hasPreviousPage > 0) {
+        this.currentIndex--;
+      } else {
+        this.$router.push({ name: 'chooseFood' })
+      }
+    },
+    next() {
+      if (this.hasNextPage) {
+        this.currentIndex++;
+      } else {
+        // TODO: Add item to order
+        this.$router.push({ name: 'orderSummary' })
+      }
+    }
+  },
+  computed: {
+    hasPreviousPage() {
+      return this.currentIndex > 0
+    },
+    hasNextPage() {
+      return this.currentIndex < this.optionals.length - 1
+    },
+    numberOfPages() {
+      return this.optionals.length
+    },
+    currentPage() {
+      return this.currentIndex + 1
+    },
+    optional() {
+      return this.optionals[this.currentIndex]
+    },
+    item() {
+      return this.$state.item
+    }
+  },
+  watch: {
+    currentIndex(to, from) {
+      const foward = to > from
+      this.slide = foward ? 'left' : 'right'
+    }
+  }
+}
+</script>
---
+++ src/views/foods/Optional.vue
@@ -0,0 +1,46 @@
+<template>
+  <SlideTransition direction="left">
+    <div>
+      <h1 class="display-3 mb-5">{{ optional.title }}</h1>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4" v-for="option in optional.options" :key="option.id">
+          <div class="form-group">
+            <label class="radio radio-primary">
+              <input type="radio" name="radio-group">
+              <span class="check-mark"></span> {{ option.name }}
+            </label>
+            <div class="pl-4">
+              <span class="pl-3" v-if="option.price > 0">
+                <small>
+                  <sup>$</sup>
+                </small>
+                {{ option.price | numeral }}
+              </span>
+              <small class="pl-3 text-muted" v-else>
+                no cost
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </SlideTransition>
+</template>
+
+<script>
+import SlideTransition from '@/transitions/SlideTransition'
+import { Optional } from '@/models'
+
+export default {
+  name: 'optional',
+  props: {
+    optional: {
+      type: Optional,
+      required: true
+    }
+  },
+  components: {
+    SlideTransition
+  }
+}
+</script>
