01b68aa3e134a7cf3b81a3fb0336ecee627aeb6c
Marx JMoura <marx.jmoura@gmail.com>
2019-08-23 20:48:12 -0300

    Add SlideUpTransition component

---
+++ src/transitions/SlideUpTransition.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition @enter="enter" @leave="leave" mode="out-in" appear>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+import { TweenMax, Power4 } from "gsap/TweenMax"
+
+export default {
+  name: 'slideUpTransition',
+  methods: {
+    enter(el, done) {
+      TweenMax.fromTo(el, .2, {
+        autoAlpha: 0,
+        yPercent: 10
+      }, {
+        autoAlpha: 1,
+        delay: .1,
+        yPercent: 0,
+        ease: Power4.easeIn,
+        onComplete: done
+      })
+    },
+    leave(el, done) {
+      TweenMax.fromTo(el, .2, {
+        autoAlpha: 1,
+        yPercent: 0
+      }, {
+        autoAlpha: 0,
+        yPercent: -10,
+        ease: Power4.easeIn,
+        onComplete: done
+      })
+    }
+  }
+}
+</script>
