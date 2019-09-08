40ca3d6e932a6eff6a5ab34dfb25dd6aa2ad9cf9
Marx JMoura <marx.jmoura@gmail.com>
2019-09-07 21:08:20 -0300

    Check if an update is available

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,11 @@
 import api from './api'
+import app from './app'
 import session from './session'
 
 export default {
   install (Vue) {
     Vue.use(api)
+    Vue.use(app)
     Vue.use(session)
   }
 }
