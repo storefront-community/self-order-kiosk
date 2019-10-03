5a7f2b5740ca1f7d912905339d22f3ef2f8aa20c
Marx JMoura <marx.jmoura@gmail.com>
2019-10-03 08:36:29 -0300

    Install FontAwesome package

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import i18n from './i18n'
 import filters from './filters'
 import plugins from './plugins'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'popper.js'
 import 'bootstrap'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(filters)
 Vue.use(plugins)
