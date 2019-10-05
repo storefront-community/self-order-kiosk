3ee3483aa77d357413a279797bf89b3107e935c7
Marx JMoura <marx.jmoura@gmail.com>
2019-10-05 10:04:31 -0300

    Remove start page title

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -2,44 +2,41 @@
   <div class="app-body">
     <div class="app-content">
       <div class="container d-flex flex-column h-100">
-        <div class="text-center">
+        <div class="text-center mb-3">
           <Logo/>
         </div>
-        <h2 class="font-weight-normal text-center d-flex flex-grow-1 align-items-center">
-          <span class="m-auto">
-            {{ $t('title') }}
-          </span>
-        </h2>
-        <div class="row">
-          <div class="col-12 col-md-6 d-flex flex-grow-1 justify-content-center justify-content-md-end">
-            <div class="d-flex flex-column mr-md-5">
-              <button type="button" class="btn btn-primary btn-lg m-auto p-5" @click="start">
-                <span class="text-uppercase">
-                  {{ $t('start_button') }}
-                </span>
-              </button>
-              <div class="text-center">
-                <button type="button" class="btn btn-flag" @click="changeLocale('br')">
-                  <span class="flag-icon flag-icon-br"></span>
+        <div class="d-flex flex-column flex-grow-1 justify-content-center">
+          <div class="row">
+            <div class="col-12 col-md-6 d-flex flex-grow-1 justify-content-center justify-content-md-end">
+              <div class="d-flex flex-column mr-md-5">
+                <button type="button" class="btn btn-primary btn-lg m-auto p-5" @click="start">
+                  <span class="text-uppercase">
+                    {{ $t('start_button') }}
+                  </span>
                 </button>
-                <button type="button" class="btn btn-flag" @click="changeLocale('en')">
-                  <span class="flag-icon flag-icon-us"></span>
-                </button>
+                <div class="text-center">
+                  <button type="button" class="btn btn-flag" @click="changeLocale('br')">
+                    <span class="flag-icon flag-icon-br"></span>
+                  </button>
+                  <button type="button" class="btn btn-flag" @click="changeLocale('en')">
+                    <span class="flag-icon flag-icon-us"></span>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-12 col-md-6 d-none d-md-flex">
-            <div class="d-flex flex-column ml-md-5">
-              <div class="d-flex justify-content-center">
-                <QRCode value="https://kiosk.storefront.community" :options="{ width: 256 }"/>
-              </div>
-              <div class="text-center d-none">
-                <small>
-                  Do not have an app?<br>
-                  <span class="text-primary">
-                    qrcode.watch
-                  </span>
-                </small>
+            <div class="col-12 col-md-6 d-none d-md-flex">
+              <div class="d-flex flex-column ml-md-5">
+                <div class="d-flex justify-content-center">
+                  <QRCode value="https://kiosk.storefront.community" :options="{ width: 256 }"/>
+                </div>
+                <div class="text-center d-none">
+                  <small>
+                    Do not have an app?<br>
+                    <span class="text-primary">
+                      qrcode.watch
+                    </span>
+                  </small>
+                </div>
               </div>
             </div>
           </div>
@@ -83,11 +80,9 @@ export default {
 <i18n>
 {
   "br": {
-    "title": "Bateu aquela fome?",
     "start_button": "Faça seu pedido"
   },
   "en": {
-    "title": "Are you hungry?",
     "start_button": "Make your order"
   }
 }
