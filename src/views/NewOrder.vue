ffc619c723f937500702a7d527302c5e8387f185
Marx JMoura <marx.jmoura@gmail.com>
2019-10-10 09:24:19 -0300

    Reduce element tree in the NewOrder component

--- src/views/NewOrder.vue
+++ src/views/NewOrder.vue
@@ -1,37 +1,35 @@
 <template>
   <SlideTransition :direction="getRouteDirection">
-    <SafeArea>
-      <div :class="`app theme-${session.theme}`" v-if="session.started">
-        <div class="app-body">
-          <div class="app-content">
-            <div class="container d-flex flex-column h-100">
-              <div class="text-center">
-                <Logo/>
-              </div>
-              <div class="d-flex flex-column flex-grow-1 justify-content-center">
-                <div class="row">
-                  <div class="col-12 col-md-6 d-flex flex-grow-1 justify-content-center justify-content-md-end">
-                    <div class="d-flex flex-column mr-md-5">
-                      <button type="button" class="btn btn-primary btn-lg m-auto p-5" @click="newOrder">
-                        <span class="text-uppercase">
-                          {{ $t('start_button') }}
-                        </span>
+    <SafeArea :class="`app theme-${session.theme}`" v-if="session.started">
+      <div class="app-body">
+        <div class="app-content">
+          <div class="container d-flex flex-column h-100">
+            <div class="text-center">
+              <Logo/>
+            </div>
+            <div class="d-flex flex-column flex-grow-1 justify-content-center">
+              <div class="row">
+                <div class="col-12 col-md-6 d-flex flex-grow-1 justify-content-center justify-content-md-end">
+                  <div class="d-flex flex-column mr-md-5">
+                    <button type="button" class="btn btn-primary btn-lg m-auto p-5" @click="newOrder">
+                      <span class="text-uppercase">
+                        {{ $t('start_button') }}
+                      </span>
+                    </button>
+                    <div class="text-center">
+                      <button type="button" class="btn btn-flag" @click="changeLocale('br')">
+                        <span class="flag-icon flag-icon-br"></span>
                       </button>
-                      <div class="text-center">
-                        <button type="button" class="btn btn-flag" @click="changeLocale('br')">
-                          <span class="flag-icon flag-icon-br"></span>
-                        </button>
-                        <button type="button" class="btn btn-flag" @click="changeLocale('en')">
-                          <span class="flag-icon flag-icon-us"></span>
-                        </button>
-                      </div>
+                      <button type="button" class="btn btn-flag" @click="changeLocale('en')">
+                        <span class="flag-icon flag-icon-us"></span>
+                      </button>
                     </div>
                   </div>
-                  <div class="col-12 col-md-6 d-none d-md-flex">
-                    <div class="d-flex flex-column ml-md-5">
-                      <div class="d-flex justify-content-center">
-                        <QRCode :value="url" :options="{ width: 256 }"/>
-                      </div>
+                </div>
+                <div class="col-12 col-md-6 d-none d-md-flex">
+                  <div class="d-flex flex-column ml-md-5">
+                    <div class="d-flex justify-content-center">
+                      <QRCode :value="url" :options="{ width: 256 }"/>
                     </div>
                   </div>
                 </div>
