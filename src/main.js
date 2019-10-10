bcb331fc975972e1557113b034c02baeca0701c3
Marx JMoura <marx.jmoura@gmail.com>
2019-10-10 17:31:18 -0300

    Replace flag-icon-css package with local SVG files

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import filters from './filters'
 import plugins from './plugins'
 import router from './router'
 import FontAwesome from './FontAwesome'
-import 'flag-icon-css/css/flag-icon.css'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
