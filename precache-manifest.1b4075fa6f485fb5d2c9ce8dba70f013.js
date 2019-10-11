e588aa36ddd68428380190ca48fdb4f8d489c63d
Circle CI <ci@circleci.services>
2019-10-11 21:14:37 +0000

    [skip ci] Deploy v0.8.0

---
+++ precache-manifest.1b4075fa6f485fb5d2c9ce8dba70f013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d7ef9ffb4c449ba67e8",
+    "url": "/css/app.d2b3bc89.css"
+  },
+  {
+    "revision": "5d7ef9ffb4c449ba67e8",
+    "url": "/js/app.f6a8e34a.js"
+  },
+  {
+    "revision": "b64b9a3517db9730338b",
+    "url": "/js/chunk-vendors.0f6652f2.js"
+  },
+  {
+    "revision": "626e0ee5f8a5beca6f262a6f8eba6335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22cc0602ae05315d8e2ad6752ce299a6",
+    "url": "/img/storefront-community.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f039f3683f081344b3df19c494777501",
+    "url": "/version.json"
+  }
+];
