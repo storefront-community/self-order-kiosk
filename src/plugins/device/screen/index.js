4e230d2a69d2c965c46f56e98f86e3ee8e836896
Marx JMoura <marx.jmoura@gmail.com>
2019-10-17 09:44:08 -0300

    Detect the screen resolution

--- src/plugins/device/screen/index.js
+++ src/plugins/device/screen/index.js
@@ -1,9 +1,11 @@
 import fullscreen from './fullscreen'
+import resolution from './resolution'
 import safeArea from './safeArea'
 import zoom from './zoom'
 
 export default {
   fullscreen,
-  zoom,
-  safeArea
+  resolution,
+  safeArea,
+  zoom
 }
