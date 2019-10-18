3ce88301c3cfc0a46361308f9febf1a59676ddd3
Marx JMoura <marx.jmoura@gmail.com>
2019-10-18 16:00:40 -0300

    Detect screen orientation

--- src/plugins/device/screen/index.js
+++ src/plugins/device/screen/index.js
@@ -1,10 +1,12 @@
 import fullscreen from './fullscreen'
+import orientation from './orientation'
 import safeArea from './safeArea'
 import size from './size'
 import zoom from './zoom'
 
 export default {
   fullscreen,
+  orientation,
   safeArea,
   size,
   zoom
